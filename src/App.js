741ee3700b9ab26b4776caa57a0988f0c6c603e2
hannan <hannanbaba26@gmail.com>
2024-02-21 17:50:36 +0530

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [name, setName] = useState("Zaid");
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" placeholder="Enter your name" onChange={handleChange}/>
+      <p>Hi {name}, How are you today?</p>
     </div>
   );
 }
